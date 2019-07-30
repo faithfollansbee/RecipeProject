49e91c5859de9ce157fb0fc4a9fcd9413a630655
faithfollansbee <faithfollansbee@gmail.com>
2019-07-30 16:44:24 -0400

    Fixed enlarge button

--- assets/scripts/recipes/events.js
+++ assets/scripts/recipes/events.js
@@ -54,6 +54,7 @@ const onDeleteRecipe = (event) => {
     .then(() => onGetRecipes(event))
     .catch(ui.failure)
 }
+
 const onGetRecipes = (event) => {
   event.preventDefault()
   console.log('here?')
@@ -66,14 +67,28 @@ const addHandlers = () => {
   $('#getRecipesButton').on('click', onGetRecipes)
   $('.content').on('click', '.delete-recipe', onDeleteRecipe)
   $('.content').on('submit', '.update-recipe', onUpdateRecipe)
-  $('.content').on('click', '.enlarge-recipe', onEnlargeRecipe)
+  $('.content').on('click', '.enlarge-recipe', function () {
+    $(this).closest('.grid-item').toggleClass('active')
+  })
 }
 
-const onEnlargeRecipe = (event) => {
-  event.preventDefault()
-  $('.grid-item').click(function () { $(this).toggleClass('active') })
-  // const recipeId = $(event.target).closest('section').data('id')
-}
+// const onEnlargeRecipe = (event) => {
+//  console.log('enlarge is running')
+//  event.preventDefault()
+//  $('.grid-item').click(function () { $(this).toggleClass('active') })
+//  $('.grid-item').click(function () { $(this).addClass('active') })
+//   $('.enlarge-recipe').text() === 'Enlarge' ? 'Close' : 'Enlarge' )
+//   $('.enlarge-recipe').hide()
+//   $('.enlarge-recipe').click(function () { $(this).text('close') })
+//   const recipeId = $(event.target).closest('section').data('id')
+//   $('.enlarge-recipe').click(function () { $(this).text() === 'Enlarge' ? 'Close' : 'Enlarge' })
+// }
+
+// const onCloseRecipe = (event) => {
+//  event.preventDefault()
+//  $('.grid-item').click(function () { $(this).toggleClass('active') })
+//   $('.enlarge-recipe').click(function () { $(this).text('enlarge') })
+// }
 
 const onUpdateRecipe = (event) => {
   event.preventDefault()
@@ -93,6 +108,6 @@ module.exports = {
   onDeleteRecipe,
   onUpdateRecipe,
   onGetRecipes,
-  addHandlers,
-  onEnlargeRecipe
+  addHandlers
+  // onCloseRecipe
 }
